ee148bf27450ba2ab11b49cf2861c793e7469eea
natasharrison <natasharrison@gmail.com>
2021-05-28 10:32:16 -0700

    express

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+// initiate the server
+const app = express();
+const apiRoutes = require('./routes/apiRoutes/index');
+const htmlRoutes = require('./routes/htmlRoutes/index');
+
+// // parse incoming string or array data
+// app.use(express.urlencoded({ extended: true }));
+// // parse incoming JSON data
+// app.use(express.json());
+// // middleware for front-end resources
+// app.use(express.static('public'));
+
+// app.use('/api', apiRoutes);
+// app.use('/', htmlRoutes);
+
+
+// makes the server listen
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
